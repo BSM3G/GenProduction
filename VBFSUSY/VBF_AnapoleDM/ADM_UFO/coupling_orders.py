a8ab083959a22b92c86421be9174d49500dc6b5e
Brenda Fabela Enriquez <bfabelae@Brendas-MacBook-Air.local>
2020-08-07 13:32:16 -0500

    Generation cards, scripts and UFO model for Anapole DM.

---
+++ VBFSUSY/VBF_AnapoleDM/ADM_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.0.30
+# Mathematica version: 9.0 for Linux x86 (32-bit) (November 20, 2012)
+# Date: Mon 12 Sep 2016 16:37:20
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
